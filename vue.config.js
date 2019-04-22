e8cb215e9e14c3ea4e1234ff9f98dbb213b3470e
wangfan <316042496@qq.com>
2019-04-22 13:33:44 +0800

    enable index.html in dist

--- vue.config.js
+++ vue.config.js
@@ -14,21 +14,22 @@ module.exports = {
   // 反向代理
   devServer: {
     // 环境配置
-    // host: '192.168.1.53',
-    // port: 8080,
-    // https: false,
-    // hotOnly: false,
-    open: true // 配置自动启动浏览器
-    // proxy: {
-    //   // 配置多个代理(配置一个 proxy: 'http://localhost:4000' )
-    //   // '/api': {
-    //   //   target: 'http://192.168.1.248:9888',
-    //   //   // target: 'http://192.168.1.4:8999',
-    //   //   pathRewrite: {
-    //   //     '^/api': '/api'
-    //   //   }
-    //   // }
-    // }
+    port: 9090,
+    open: true, // 配置自动启动浏览器
+    proxy: {
+      '/deedSrock': {
+        target: 'https://www.deadstock.ca',
+        pathRewrite: {
+          '^/deedSrock': ''
+        }
+      },
+      '/socialStatus': {
+        target: 'https://www.socialstatuspgh.com',
+        pathRewrite: {
+          '^/socialStatus': ''
+        }
+      }
+    }
   },
   pluginOptions: {
     // 第三方插件配置
